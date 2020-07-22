733c650907a17e36b2b09cda2af79c937fa304e5
FayeCarter <fayecarter333@gmail.com>
2020-07-22 20:38:42 +0100

    Hompepage renders Search Component

--- frontend/src/components/Homepage.vue
+++ frontend/src/components/Homepage.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="homepage">
     <h1>Tails Store Locator</h1>
+    <Search />
   </div>
 </template>
 
 <script>
+import Search from "./Search.vue";
+
 export default {
-  name: 'Homepage'
-}
+  name: "Homepage",
+  components: {
+    Search,
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- frontend/tests/unit/Homepage.spec.js
+++ frontend/tests/unit/Homepage.spec.js
@@ -1,9 +1,15 @@
 import { shallowMount } from '@vue/test-utils'
 import Homepage from '@/components/Homepage.vue'
+import Search from '@/components/Search.vue'
 
 describe('Homepage.vue', () => {
   it('renders Store locator heading', () => {
     const wrapper = shallowMount(Homepage)
     expect(wrapper.text()).toEqual("Tails Store Locator")
+  })
+
+  it('renders props.msg when passed', () => {
+    const wrapper = shallowMount(Homepage)
+    expect(wrapper.findComponent(Search).exists()).toBe(true)
   })
 })
