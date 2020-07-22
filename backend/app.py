9efaee25cb85ec5c7a72e36c4239ff81d745fcfc
FayeCarter <fayecarter333@gmail.com>
2020-07-22 21:38:34 +0100

    Add cors to flask app

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from stores.stores import stores_bp
 
@@ -6,6 +7,7 @@ app = Flask(__name__)
 if __name__ == '__main__':
   app.run(debug=True)
 
+cors = CORS(app, resources={r"/stores/*": {"origins": "*"}})
 app.register_blueprint(stores_bp, url_prefix='/stores')
 
 @app.route('/')
