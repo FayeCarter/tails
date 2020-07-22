fcd2093aa520bd557d8291cc9987afb18d987ddb
FayeCarter <fayecarter333@gmail.com>
2020-07-22 11:31:54 +0100

    Render place name on name search

--- backend/stores/stores.py
+++ backend/stores/stores.py
@@ -1,7 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+import json
+with open('../stores.json', 'r') as stores_file:
+  data = stores_file.read()
+stores_list = json.loads(data)
 
 stores_bp = Blueprint('stores', __name__)
 
 @stores_bp.route('/search', methods=["GET"])
 def search():
-  return "Search"
+  query = request.args.get('query')
+  response = ""
+
+  if query:
+    for store in stores_list:
+      if query in store['name']:
+        response = store['name']
+
+    return jsonify(response)
+
+  else:
+    response = 'Invalid search'
+    return response
--- backend/tests/test_stores.py
+++ backend/tests/test_stores.py
@@ -1,3 +1,18 @@
-def test_search(app, client):
+import json
+
+def test_search_route(app, client):
+  """
+  Stores search route is reachable
+  """
   response = client.get('/stores/search')
   assert response.status_code == 200
+  assert response.data == b'Invalid search'
+
+def test_search_function(app, client):
+  """
+  When searching for a place name
+  The stores search route returns json result
+  """
+  response = client.get('/stores/search?query=hav')
+  assert response.status_code == 200
+  assert response.json == 'Newhaven'
