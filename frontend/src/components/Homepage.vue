7b952200dca1a122700ef5c584cd12183e54437e
FayeCarter <fayecarter333@gmail.com>
2020-07-23 08:46:48 +0100

    Return results on submit

--- frontend/src/components/Homepage.vue
+++ frontend/src/components/Homepage.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="homepage">
     <h1>Tails Store Locator</h1>
-    <Search />
+    <Search @storesFound="onStoresFound" />
+    <div v-if="stores">
+      <Store v-for="store in stores" v-bind:key="store" :name="store" />
+    </div>
   </div>
 </template>
 
 <script>
 import Search from "./Search.vue";
+import Store from "./Store.vue";
 
 export default {
   name: "Homepage",
   components: {
     Search,
+    Store
   },
+    data() {
+    return {
+      stores: []
+    }
+  },
+  methods: {
+    onStoresFound(value) {
+      this.stores = value
+    }
+  }
 };
 </script>
 
